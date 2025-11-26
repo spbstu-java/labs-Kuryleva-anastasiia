0eec4a91734b81958b8508ae8526cb99ab2f0a38
Kuryleva Anastasia <knastya76@yandex.ru>
2025-11-26 17:17:08 +0300

    Lab 3 ready

---
+++ src/Lab_3/Main.java
@@ -0,0 +1,9 @@
+import Lab_3.Translator;
+
+// Для теста: dog look to the window, dog look forward ! dog look forward, dog look to the window,
+public static void main(String[] args) {
+    Translator translator = new Translator();
+    translator.run();
+}
+
+
